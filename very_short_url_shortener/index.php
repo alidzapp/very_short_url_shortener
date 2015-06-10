1db79d72e0f3c3cee8fcddac45848e8ba9bc0f27
webdev23 <contact@webdev23.com>
2015-06-10 22:26:53 +0200

    First commit

---
+++ very_short_url_shortener/index.php
@@ -0,0 +1,75 @@
+<?
+
+//~ Very short url shortener!
+
+$toshort = $_POST["bigurl"];
+
+$host = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";;
+
+$bookmarklet = 'javascript:void(window.location="'.$host.'?to_short="+encodeURIComponent(window.location.href));';
+
+if (empty ($toshort)) {
+
+	echo'<a href="index.php">Hello!</a><br>I am very short url shortener.<br>
+
+	I need urls to exist.<br>Feed me now.<br>Or use <a href="" onclick="alrt()">the bookmarklet</a>
+
+    ';
+
+    echo'<textarea style="display:none" id="bktxt">'.$bookmarklet.'</textarea>';
+
+	echo'<form method="post">
+
+	<input type="input" name="bigurl" placeholder="//url" value="'.$_REQUEST["to_short"].'"/>
+
+	<input type="submit" value="Shorten with very short url shortener" id="shr"/>
+
+	</form>';
+
+	};
+
+if (!empty ($toshort)) {	
+
+	$token = bin2hex(mcrypt_create_iv(3, MCRYPT_DEV_RANDOM));
+
+	file_put_contents($token, $toshort . PHP_EOL, FILE_APPEND);
+
+	echo $toshort;
+
+	echo'<br>has been shorten into:<br>';
+
+	echo'<a target="blank" href="?s='.$token.'">?s='.$token.'</a>';
+
+    };
+
+if (!empty ($_GET["s"])) {	
+
+	echo"<br>";
+
+	$sid = $_GET["s"];
+
+	$linked = file_get_contents($sid);
+
+	if (empty ($linked)) {
+
+    echo"Error 404 gone to party.";
+
+	};
+
+	if (!empty ($linked)) {			
+
+    echo'<a href="'.$linked.'" id="lnk">'.$linked.'</a>';
+
+    echo'<script>document.getElementById("lnk").click();</script>';
+
+    }};
+
+if (!empty ($_REQUEST["to_short"])) {
+
+	echo'<script>document.getElementById("shr").click();</script>';	
+
+	};
+
+?>
+
+<script>function alrt() {alert(document.getElementById("bktxt").innerHTML)};</script>
