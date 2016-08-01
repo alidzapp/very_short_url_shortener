b2a03c93e995b9f626052759f5f0165a71eeee51
webdev23 <webdev23@users.noreply.github.com>
2016-08-01 02:29:42 +0200

    Add a bit of style

--- very_short_url_shortener/index.php
+++ very_short_url_shortener/index.php
@@ -1,6 +1,6 @@
 <?php
 
-//~ Very short url shortener!
+//~ Very short url shortener! 
 
 ini_set('display_errors', 'Off');
 // error_reporting(E_ALL);
@@ -12,11 +12,13 @@ $host = "http://$_SERVER[HTTP_HOST]$_SERVER[SCRIPT_NAME]";
 
 $bookmarklet = 'javascript:void(window.location=&#39;'.$host.'?to_short=&#39+encodeURIComponent(window.location.href));';
 
+echo '<meta name="viewport" content="width=device-width, initial-scale=4"><h1>';
+
 if (empty ($toshort)) {
 
-	echo'<a href="index.php">Hello!</a><br>I am very short url shortener.<br>
+	echo'<a href="index.php">Hello!</a><br>I am very short url shortener<br>
 
-	I need urls to exist.<br>Feed me now.<br>Or use <a href="'.$bookmarklet.'" onclick="alrt()">the bookmarklet</a>
+	I need urls to exist<br><i>Feed me now</i><br>Or use <a href="'.$bookmarklet.'" onclick="alrt()">the bookmarklet</a>!<p>
 
     ';
 
@@ -24,9 +26,9 @@ if (empty ($toshort)) {
 
 	echo'<form method="post">
 
-	<input type="input" name="bigurl" placeholder="//url" value="'.$_REQUEST["to_short"].'"/>
+	<input type="input" name="bigurl" placeholder="//url" value="'.$_REQUEST["to_short"].'"/><br>
 
-	<input type="submit" value="Shorten with very short url shortener" id="shr"/>
+	<input type="submit" value="Shorten with_very_short_url_shortener" id="shr"/>
 
 	</form>';
 
@@ -34,17 +36,17 @@ if (empty ($toshort)) {
 
 if (!empty ($toshort)) {	
 
-// Change the 5 to match your choice of url
+// Change the 2 to match your choice
 
-	$token = bin2hex(openssl_random_pseudo_bytes(3));
+	$token = bin2hex(openssl_random_pseudo_bytes(2));
 
 	file_put_contents($token, $toshort . PHP_EOL, FILE_APPEND);
 
 	echo $toshort;
 
-	echo'<br>has been shorten into:<br>';
+	echo'<h1><br>has been shorten into:<br>';
 
-	echo'<a href="?s='.$token.'">?s='.$token.'</a>';
+	echo'<a href="?s='.$token.'">?s='.$token.'</a></h1>';
 
     };
 
@@ -75,7 +77,13 @@ if (!empty ($_REQUEST["to_short"])) {
 	echo'<script>document.getElementById("shr").click();</script>';	
 
 	};
-
 ?>
 
-<script>function alrt() {alert(document.getElementById("bktxt").innerHTML)};</script>
+	<script>
+	function alrt() {alert(document.getElementById("bktxt").innerHTML)};
+	</script>
+	
+	<style>
+	body{text-align:center;font:caption;background-color:#f5b041;padding-top:12%;transform:scale(2)}
+	</style>
+	
